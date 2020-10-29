45c94b36fa8723cb88a5dea5a4ddc0993b346ad4
Sovann Seung <scienceboy.sovann@gmail.com>
2020-10-28 21:07:31 -0400

    Add load balance

--- src/main/java/com/javatechie/report/SpringBootJasperReportApplication.java
+++ src/main/java/com/javatechie/report/SpringBootJasperReportApplication.java
@@ -2,8 +2,7 @@ package com.javatechie.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,20 +10,19 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
 public class SpringBootJasperReportApplication {
 
+    @LoadBalanced
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
     public static void main(String[] args) {
-
         SpringApplication.run(SpringBootJasperReportApplication.class, args);
     }
 
